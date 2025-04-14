fbdfc26ed98e64bb2c941109e0a762f7d8253ffc
Alice Mugure <machariamugure17@gmail.com>
2025-04-14 13:46:51 +0300

    Initial Commit

---
+++ src/Components/Table.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Table() {
+  return (
+    <div>
+        <table>
+    <thead>
+        <tr>
+            <th>Expense</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+            <th>Date</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+    </div>
+
+
+
+  );
+}
+
+export default Table;
